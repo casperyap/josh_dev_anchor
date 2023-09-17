d548609c82e611192ae9afb373578ab069d9bc0b
Casper Yap <casyap@gmail.com>
2023-09-17 07:25:32 +0000

    Modify basic-1 to implement with operations demonstrated in Josh's DevBox video.

--- examples/tutorial/basic-1/programs/basic-1/src/lib.rs
+++ examples/tutorial/basic-1/programs/basic-1/src/lib.rs
@@ -1,26 +1,39 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+// declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("BZBTgGa8thX4LbZYMdsBcLVTy9zimwU17UYzgRtGewzq");
 
 #[program]
 mod basic_1 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>, data: u64) -> Result<()> {
+    pub fn initialize(ctx: Context<InitializeAccounts>) -> Result<()> {
         let my_account = &mut ctx.accounts.my_account;
-        my_account.data = data;
+        my_account.data = 0;
         Ok(())
     }
 
-    pub fn update(ctx: Context<Update>, data: u64) -> Result<()> {
+    pub fn set(ctx: Context<UpdateAccounts>, data: u64) -> Result<()> {
         let my_account = &mut ctx.accounts.my_account;
         my_account.data = data;
         Ok(())
     }
+    
+    pub fn increment(ctx: Context<UpdateAccounts>) -> Result<()> {
+      let my_account = &mut ctx.accounts.my_account;
+      my_account.data += 1;
+      Ok(())
+  }
+  
+  pub fn decrement(ctx: Context<UpdateAccounts>) -> Result<()> {
+    let my_account = &mut ctx.accounts.my_account;
+    my_account.data -= 1; //should check that counter is not zero.
+    Ok(())
+}
 }
 
 #[derive(Accounts)]
-pub struct Initialize<'info> {
+pub struct InitializeAccounts<'info> {
     #[account(init, payer = user, space = 8 + 8)]
     pub my_account: Account<'info, MyAccount>,
     #[account(mut)]
@@ -29,7 +42,7 @@ pub struct Initialize<'info> {
 }
 
 #[derive(Accounts)]
-pub struct Update<'info> {
+pub struct UpdateAccounts<'info> {
     #[account(mut)]
     pub my_account: Account<'info, MyAccount>,
 }
